3f5f5039c738c7288298a86fac1442535c4aa72a
kishanV2321 <kv230801@gmail.com>
2024-08-26 19:32:52 +0530

    body unneccessary code remove

--- src/pages/Body.tsx
+++ src/pages/Body.tsx
@@ -1,30 +1,7 @@
 import { Outlet } from "react-router-dom";
 import { Header } from "../components";
-// import { useEffect } from "react";
-// import { useDispatch } from "react-redux";
 
 const Body = () => {
-    // const dispatch = useDispatch();
-    // const navigate = useNavigate();
-
-    // useEffect(() => {
-    //     // Replace with your actual authentication check
-    //     const user = null; // Placeholder for actual user object
-    //     if (user) {
-    //         const { id, name, email, photoURL } = user;
-    //         dispatch(
-    //             addUser({
-    //                 id: id,
-    //                 name: name,
-    //                 email: email,
-    //                 photoURL: photoURL,
-    //             })
-    //         );
-    //     } else {
-    //         dispatch(removeUser());
-    //         navigate("/");
-    //     }
-    // }, [dispatch, navigate]);
 
     return (
         <div>
